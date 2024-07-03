b2199382af2a183184e3e127509edeab575cca5c
kayra1 <gemalmazkayra@gmail.com>
2024-07-03 11:26:24 +0300

    lint

--- tests/integration/test_lepy.py
+++ tests/integration/test_lepy.py
@@ -1,22 +1,10 @@
 import os
 import subprocess
 import time
-from typing import Optional
 
 import pytest
 import requests
 from lepy import run_lego_command
-
-# Set up Go
-# Add ~/go/bin to your $PATH, or set GOBIN to a directory that is in your $PATH already,
-# One way to do this is to add export PATH=$PATH:$HOME/go/bin to your ~/.profile
-# cd pebble
-# go install ./cmd/pebble
-
-# temporarily trust /test/certs/pebble.minica.pem
-# Root CA certificate is also available at: https://0.0.0.0:15000/roots/0
-# export SSL_CERT_FILE=/home/kayra/Documents/work/canonical/lepy/tests/integration/pebble/test/certs/pebble.minica.pem
-# run pebble -config ./test/config/pebble-config.json -strict false
 
 
 @pytest.fixture(scope="session")
@@ -31,7 +19,6 @@ def configure_acme_server():
         cwd=os.path.join(tests_dir, "pebble"),
     )
 
-    # Get the CA cert to trust TODO: this doesn't work
     ca_path = os.path.join(tests_dir, "pebble/test/certs/pebble.minica.pem")
     filename = os.path.join(tests_dir, "test_files/test.csr")
     localhost_csr = open(filename).read().encode()
